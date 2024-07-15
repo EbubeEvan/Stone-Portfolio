bb9ffdbdbd784a197d1e4b9c13f4fea83841be8e
unknown <ebubemmayie@gmail.com>
2024-07-15 23:50:57 +0100

    optimized again

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,7 +1,6 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
@@ -9,7 +8,8 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      {/* <hemisphereLight intensity={0.15} groundColor='black' /> */}
+      <directionalLight args={[20, 3, 5]}/>
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
