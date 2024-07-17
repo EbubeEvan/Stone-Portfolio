569e53ffdb673de150f00e24c226d6978253bf1b
unknown <ebubemmayie@gmail.com>
2024-07-17 09:39:59 +0100

    made more adjustments to animations

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -7,7 +7,6 @@ import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
 
 const FeedbackCard = ({
-  index,
   testimonial,
   name,
   designation,
@@ -15,7 +14,7 @@ const FeedbackCard = ({
   image,
 }) => (
   <motion.div
-    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    variants={fadeIn("", "spring", 0, 0)}
     className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
   >
     <p className='text-white font-black text-[48px]'>"</p>
@@ -55,8 +54,8 @@ const Feedbacks = () => {
         </motion.div>
       </div>
       <div className={`-mt-20 pb-14 ${styles.paddingX} flex flex-wrap gap-7`}>
-        {testimonials.map((testimonial, index) => (
-          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
+        {testimonials.map((testimonial) => (
+          <FeedbackCard key={testimonial.name} {...testimonial} />
         ))}
       </div>
     </div>
